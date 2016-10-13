a9fc581dbf726ba383994e912aa1303c40f94cce
Sebastian Willenborg <comawill@wlnbrg.de>
2016-10-14 00:46:26 +0200

    provide unittesting via travis-ci

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 import os, sys
 
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, Command
 try:
    from distutils.command.build_py import build_py_2to3 as build_py
 except ImportError:
    from distutils.command.build_py import build_py
-
+import unittest
 
 classifiers = """\
 Intended Audience :: Developers
@@ -32,12 +32,27 @@ if major == 3:
 def read(*rel_names):
     return open(os.path.join(os.path.dirname(__file__), *rel_names)).read()
 
+class TestCommand(Command):
+    user_options = []
+    def initialize_options(self):
+        sys.path.insert(0, './src')
+        pass
+
+    def finalize_options(self):
+        pass
+    def run(self):
+        test_suite = unittest.TestLoader().discover('./src/slimit/tests', pattern='test_*.py')
+        test_results = unittest.TextTestRunner(verbosity=2).run(test_suite)
+        if not test_results.wasSuccessful():
+            sys.exit(1)
+
 
 setup(
     name='slimit',
     version='0.8.1',
     url='https://slimit.readthedocs.io',
-    cmdclass = {'build_py': build_py},
+    cmdclass = {'build_py': build_py,
+        'test': TestCommand},
     license='MIT',
     description='SlimIt - JavaScript minifier',
     author='Ruslan Spivak',
